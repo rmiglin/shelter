a9489983da5568d735ab868b0a68c40472e070b8
Karla <kpadron1@hotmail.com>
2017-11-21 23:52:24 -0600

    general setting load

--- Alpha/HomeViewController.swift
+++ Alpha/HomeViewController.swift
@@ -19,77 +19,23 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
     var userList = [UserModel]()
     var refUsers: DatabaseReference!
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 4
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        //creating a cell using the custom class
-        var theUser: UserModel
-        theUser = userList.first!
-        
-        let cell1 = tableView.dequeueReusableCell(withIdentifier: "General", for: indexPath) as! GeneralTableViewCell
-        
-        if indexPath.row == 0{
-        let cell = tableView.dequeueReusableCell(withIdentifier: "General", for: indexPath) as! GeneralTableViewCell
-            cell.email.text = theUser.email
-            cell.name.text = theUser.firstName
-            cell.phone.text = theUser.phoneNumber
-            cell.username.text = theUser.status
-            if theUser.status == "green"{
-                cell.username.text = "safe"
-            }
-            if theUser.status == "red"{
-                cell.username.text = "unsafe"
-            }
-            cell.password.text = theUser.password
-            return cell
-        }
-        if indexPath.row == 1{
-             let cell = tableView.dequeueReusableCell(withIdentifier: "Privacy", for: indexPath)
-            return cell
-        }
-        if indexPath.row == 2{
-             let cell = tableView.dequeueReusableCell(withIdentifier: "Location", for: indexPath)
-            return cell
-        }
-        if indexPath.row == 3{
-             let cell = tableView.dequeueReusableCell(withIdentifier: "Notifications", for: indexPath)
-            return cell
-        }
-        return cell1
-    }
-    
-
-    @IBAction func logout(_ sender: Any) {
-        if Auth.auth().currentUser != nil {
-            do {
-                try Auth.auth().signOut()
-                let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "Login")
-                present(vc, animated: true, completion: nil)
-                
-            } catch let error as NSError {
-                print(error.localizedDescription)
-                
-            }
-        }
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Home"
-        self.settingTableView.reloadData()
         // Do any additional setup after loading the view.
         refUsers = Database.database().reference().child("users");
         
+        //self.userList = UserModel()
+        
         //observing the data changes
+        userList = [UserModel(id: "", firstName: "", lastName: "", email: "", password: "", phoneNumber: "", streetAddress: "", city: "", state: "", zip: "", status: "")]
         refUsers.observe(DataEventType.value, with: { (snapshot) in
             
             //if the reference have some values
             if snapshot.childrenCount > 0 {
                 
                 //clearing the list
-                self.userList.removeAll()
+                //self.userList.removeAll()
                 
                 //iterating through all the values
                 for user in snapshot.children.allObjects as! [DataSnapshot] {
@@ -112,22 +58,85 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
                                          status: userStatus as! String?)
                     
                     //appending it to list
-                    self.userList.append(user)
                     
+                    if(user.email! == Auth.auth().currentUser!.email! ) {
+                        self.userList = [user]
+                        self.settingTableView.reloadData()
+                    }
                 }
-                self.settingTableView.reloadData()
+                
             }
             
         })
     }
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //creating a cell using the custom class
+        let cell1 = tableView.dequeueReusableCell(withIdentifier: "General", for: indexPath) as! GeneralTableViewCell
+        
+        var theUser: UserModel
+        print(userList)
+        theUser = userList[0]
+        
+        
+        
+        if indexPath.row == 0{
+            let cell = tableView.dequeueReusableCell(withIdentifier: "General", for: indexPath) as! GeneralTableViewCell
+            
+            cell.email.text = theUser.email
+            cell.name.text = theUser.firstName
+            cell.phone.text = theUser.phoneNumber
+            cell.username.text = theUser.status
+            
+            if theUser.status == "green"{
+                cell.username.text = "safe"
+            }
+            if theUser.status == "red"{
+                cell.username.text = "unsafe"
+            }
+            cell.password.text = theUser.password
+            
+            return cell
+        }
+        if indexPath.row == 1{
+            let cell = tableView.dequeueReusableCell(withIdentifier: "Privacy", for: indexPath)
+            return cell
+        }
+        if indexPath.row == 2{
+            let cell = tableView.dequeueReusableCell(withIdentifier: "Location", for: indexPath)
+            return cell
+        }
+        if indexPath.row == 3{
+            let cell = tableView.dequeueReusableCell(withIdentifier: "Notifications", for: indexPath)
+            return cell
+        }
+        return cell1
+    }
+    
+    
+    @IBAction func logout(_ sender: Any) {
+        if Auth.auth().currentUser != nil {
+            do {
+                try Auth.auth().signOut()
+                let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "Login")
+                present(vc, animated: true, completion: nil)
+                
+            } catch let error as NSError {
+                print(error.localizedDescription)
+                
+            }
+        }
+    }
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        //clear map of annotations
-        self.settingTableView.reloadData()
-    }
 }
+
